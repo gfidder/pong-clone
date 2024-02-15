10d29e3ad933250212f78dcea30fcec9873b570e
Gabe Fidder <gabfid@gmail.com>
2024-02-14 22:56:42 -0600

    fixing bug and adding collision

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    sprite::collide_aabb::{collide, Collision},
+};
 use paddle::{Paddle, PaddleBundle, PaddleLocation, PADDLE_PADDING};
 use std::time::Duration;
 
@@ -92,10 +95,19 @@ impl WallBundle {
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .insert_resource(Time::<Fixed>::from_duration(Duration::from_millis(10)))
+        .insert_resource(Time::<Fixed>::from_duration(Duration::from_millis(5)))
         .add_systems(Startup, setup)
-        .add_systems(Update, (player_paddle_movement, run_cpu_logic))
-        .add_systems(FixedUpdate, apply_velocity)
+        .add_systems(
+            FixedUpdate,
+            (
+                player_paddle_movement,
+                run_cpu_logic,
+                apply_velocity,
+                check_collision,
+            )
+                .chain(),
+        )
+        // .add_systems(FixedUpdate, apply_velocity)
         .run();
 }
 
@@ -109,10 +121,18 @@ fn setup(mut commands: Commands) {
 
     commands.spawn((
         Ball,
-        Velocity(Vec2::new(0.0, 0.0)),
+        Velocity(Vec2::new(400.0, 00.0)),
         Collider,
-        Transform {
-            translation: Vec3::new(0.0, 0.0, 0.0),
+        SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 0.0),
+                scale: Vec3::new(15.0, 15.0, 1.0),
+                ..Default::default()
+            },
+            sprite: Sprite {
+                color: Color::RED,
+                ..Default::default()
+            },
             ..Default::default()
         },
     ));
@@ -179,7 +199,7 @@ fn player_paddle_movement(
     player_transform.translation.y = new_transform.clamp(lower_bound, upper_bound);
 }
 
-fn apply_velocity(mut query: Query<(&mut Transform, &Velocity), Without<Ball>>, time: Res<Time>) {
+fn apply_velocity(mut query: Query<(&mut Transform, &Velocity), With<Ball>>, time: Res<Time>) {
     for (mut transform, velocity) in &mut query {
         transform.translation.x += velocity.x * time.delta_seconds();
         transform.translation.y += velocity.y * time.delta_seconds();
@@ -187,10 +207,12 @@ fn apply_velocity(mut query: Query<(&mut Transform, &Velocity), Without<Ball>>, 
 }
 
 fn run_cpu_logic(
-    ball_query: Query<(&Velocity, &Transform), With<Ball>>,
-    mut cpu_query: Query<&mut Transform, (With<CpuPlayer>, Without<Player>)>,
+    ball_query: Query<(&Velocity, &Transform), (With<Ball>, Without<Paddle>)>,
+    mut cpu_query: Query<&mut Transform, (With<CpuPlayer>, Without<Ball>)>,
     time: Res<Time>,
 ) {
+    // debug this here: https://github.com/bevyengine/bevy/issues/2198
+
     let (ball_velocity, ball_transform) = ball_query.single();
     let mut cpu_transform = cpu_query.single_mut();
     let mut direction = 0.0;
@@ -205,6 +227,49 @@ fn run_cpu_logic(
     } else if ball_transform.translation.y < cpu_transform.translation.y {
         direction -= 1.0;
     }
+
+    let new_cpu_transform = cpu_transform.translation.y + direction * 100.0 * time.delta_seconds();
+
+    let upper_bound = WALL_TOP - WALL_THICKNESS / 2.0 - 100.0 / 2.0 - PADDLE_PADDING;
+    let lower_bound = WALL_BOTTOM + WALL_THICKNESS / 2.0 + 100.0 / 2.0 + PADDLE_PADDING;
+
+    cpu_transform.translation.y = new_cpu_transform.clamp(lower_bound, upper_bound);
 }
 
-// fn check_collision(mut paddle_query)
+fn check_collision(
+    mut ball_query: Query<(&mut Velocity, &Transform), With<Ball>>,
+    collider_query: Query<&Transform, With<Collider>>,
+) {
+    let (mut ball_velocity, ball_transform) = ball_query.single_mut();
+    let ball_size = ball_transform.scale.truncate();
+
+    for collider in &collider_query {
+        let collision = collide(
+            ball_transform.translation,
+            ball_size,
+            collider.translation,
+            collider.scale.truncate(),
+        );
+
+        if let Some(collision) = collision {
+            let mut reflect_x = false;
+            let mut reflect_y = false;
+
+            match collision {
+                Collision::Left => reflect_x = ball_velocity.x > 0.0,
+                Collision::Right => reflect_x = ball_velocity.x < 0.0,
+                Collision::Bottom => reflect_y = ball_velocity.y > 0.0,
+                Collision::Top => reflect_y = ball_velocity.y < 0.0,
+                _ => {}
+            }
+
+            if reflect_y {
+                ball_velocity.y = -ball_velocity.y
+            }
+
+            if reflect_x {
+                ball_velocity.x = -ball_velocity.x
+            }
+        }
+    }
+}
